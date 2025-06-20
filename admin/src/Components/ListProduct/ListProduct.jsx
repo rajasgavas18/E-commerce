caf691f4aa598c2080c2f7187a006a7614df6a87
rajasgavas18 <149710542+rajasgavas18@users.noreply.github.com>
2025-06-20 23:23:06 +0530

    Update ListProduct.jsx

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -9,7 +9,7 @@ const ListProduct = () => {
     const [allproducts,setAllProducts] = useState([]);
 
     const fetchInfo = async ()=>{
-        await fetch('http://localhost:4000/allproducts')
+        await fetch('https://e-commerce-backend-twl7.onrender.com/allproducts')
         .then((res)=>res.json())
         .then((data)=>{setAllProducts(data)});
     }
@@ -19,7 +19,7 @@ const ListProduct = () => {
     },[])
 
     const remove_product = async (id)=>{
-        await fetch('http://localhost:4000/removeproduct',{
+        await fetch('https://e-commerce-backend-twl7.onrender.com/removeproduct',{
             method: 'POST',
             headers:{
                 Accept:'application/json',
