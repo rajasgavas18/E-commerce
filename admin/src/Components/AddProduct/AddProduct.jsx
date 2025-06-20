a36e2b5fb09135f6db2cf7f616c11c776d3932e7
rajasgavas18 <149710542+rajasgavas18@users.noreply.github.com>
2025-06-20 23:21:34 +0530

    Update AddProduct.jsx

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -29,7 +29,7 @@ const Add_product = async ()=>{
     let formData = new FormData();
     formData.append('product',image);
 
-    await fetch('http://localhost:4000/upload',{
+    await fetch('https://e-commerce-backend-twl7.onrender.com/upload',{
         method:'POST',
         headers:{
             Accept:'application/json'
@@ -41,7 +41,7 @@ const Add_product = async ()=>{
     {
         product.image = responseData.image_url;
         console.log(product);
-        await fetch('http://localhost:4000/addproduct',{
+        await fetch('https://e-commerce-backend-twl7.onrender.com/addproduct',{
             method:'POST',
             headers:{
             Accept:'application/json',
