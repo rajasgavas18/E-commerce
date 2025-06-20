589b4026edabb9957e17cec88e2ba1630ca94774
rajasgavas18 <149710542+rajasgavas18@users.noreply.github.com>
2025-06-21 01:31:43 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -35,7 +35,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 // Creating upload endpoint for images
-app.use('/images', express.static('upload/images'));
+app.use("/images", express.static("upload/images"));
 app.post("/upload", upload.single("product"), (req, res) => {
   res.json({
     success: 1,
